157ba5e1566295c13983486328582c45a804a1a7
Darshan Manavadariya <91521910+ManavadriyaDarshan@users.noreply.github.com>
2025-01-10 16:59:41 +0530

    Delete Janitri directory

--- Janitri/src/main/java/com/example/Janitri/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Janitri.repository;
-
-import com.example.Janitri.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
